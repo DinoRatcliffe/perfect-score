b2f2a0d34b7d838d493bf32e0d2a6f27902cfb4c
Dino Ratcliffe <yo@dino.io>
2015-03-16 14:03:01 +0000

    fixed routing

--- client/app/app.js
+++ client/app/app.js
@@ -29,7 +29,7 @@ angular.module('perfectScoreApp', [
       // Intercept 401s and redirect you to login
       responseError: function(response) {
         if(response.status === 401) {
-          $location.path('/login');
+          $location.path('/');
           // remove any stale tokens
           $cookieStore.remove('token');
           return $q.reject(response);
